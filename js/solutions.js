c79fcb5b11467d881e3a7a5442c0744f0c129c89
AllanMorales94 <overlydedicated1994@gmail.com>
2023-05-22 16:23:12 -0700

    Use of function expression

--- js/solutions.js
+++ js/solutions.js
@@ -1,4 +1,5 @@
-// 1.
+// 1. Define a function, as a function declaration, maxOfTwoNumbers that takes two numbers as arguments and returns the largest of them. If they are the same, return that number. Use the if-else construct or a ternary expression - the Math.max method is not allowed.
+
 function maxOfTwoNumbers(x, y) {
   if (x >= y) {
     return x;
@@ -12,4 +13,30 @@ function maxOfTwoNumbers(x, y) {
 
 console.log(maxOfTwoNumbers(3, 9));
 
-// 2.
+// 2. Define a function, as a function expression, maxOfThree that takes three numbers as arguments and returns the largest of them. Again, the Math.max method is not allowed.
+
+const maxOfThree = function (x, y, z) {
+if (x > y) {
+  if (x > z) {
+    return x;
+  } else {
+    return z;
+  }
+} else {
+  if (y > z) {
+    return y;
+  } else {
+    return z;
+  }
+}
+};
+
+// if(x >= y && x >= z){
+//   return x;
+// }else if(y >= x && y >= z){
+//   return y;
+// }else{
+//   return z;
+// };
+
+console.log(maxOfThree(3, 9, 10));
