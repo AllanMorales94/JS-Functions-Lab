2616d01b46aa536d071444940a95e751f1a4e5f0
AllanMorales94 <overlydedicated1994@gmail.com>
2023-05-23 18:55:23 -0700

    Use of a for loop to iterate through an array of numbers

--- js/solutions.js
+++ js/solutions.js
@@ -49,3 +49,15 @@ function isCharAVowel(char) {
 }
 
 console.log(isCharAVowel('a'));
+
+// 4. Define a function, as a function expression, sumArray that takes an array of numbers and returns the sum of those numbers. For example, sumArray([2, 4, 5]); would return 11.
+
+const sumArray = function(array) {
+let sum = 0;
+for(let i = 0; i < array.length; i++) {
+  sum += array[i];
+}
+return sum;
+}
+
+console.log(sumArray([10, 10, 10]));
